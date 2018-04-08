8319026ec5ba90dd1dc1c39cdad4014fd399dada
HorolLooloo <jsh05042@gmail.com>
2018-04-08 13:07:11 +0900

    draw block

--- lab1/embedded_pyGame/Crash_Of_Block.py
+++ lab1/embedded_pyGame/Crash_Of_Block.py
@@ -4,6 +4,7 @@
 # bounce ball
 # draw bar
 # control bar
+# draw block
 
 import pygame, sys
 from pygame.locals import *
@@ -22,15 +23,45 @@ by = height / 2
 dx = 1
 dy = 1
 
+bricks_cols = 3
+bricks_rows = 7
+brickWidth = 75
+brickHeight = 20
+brickPadding = 10
+brickOffsetTop = 30
+brickOffsetLeft = 30
+
 px = 0
 py = 440
 p_width = 80
 p_height = 10
+
 keys = [False, False]
+bricks = []
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Crash_Of_Block')
+
+
+def bricksInit():
+    global bricks_cols, bricks_rows, brickWidth, brickHeight, brickPadding, brickOffsetTop, brickOffsetLeft
+
+    for r in range(bricks_rows):
+        for c in range(bricks_cols):
+            state = 1
+            x = brickOffsetLeft + r*(brickPadding + brickWidth)
+            y = brickOffsetTop + c*(brickPadding + brickHeight)
+            oneBrick = [x, y, state]
+            bricks.append(oneBrick)
+
+    print(bricks)
+
+def drawBricks():
+    global bricks_cols, bricks_rows, brickWidth, brickHeight, brickPadding, brickOffsetTop, brickOffsetLeft
+    for b in bricks:
+        pygame.draw.rect(screen, GREEN, (b[0], b[1],brickWidth, brickHeight))
+
 
 def drawbar(x, y):
     pygame.draw.rect(screen, WHITE, (px, py, p_width, p_height))
@@ -66,6 +97,7 @@ def collideCheck():
         #dx *= -1
         dy *= -1
 
+bricksInit()
 
 while True:
     screen.fill(BLACK)
@@ -87,6 +119,7 @@ while True:
             elif event.key == K_RIGHT:
                 keys[1] = False
 
+    drawBricks()
     updateObject()
     collideCheck()
     drawball(bx, by, radius)
